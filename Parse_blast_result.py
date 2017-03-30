f17dd997726a6e129b3891dd84f24447c8894e07
Chao-Z <Chao-Z@users.noreply.github.com>
2017-03-30 21:50:22 +0800

    Update Parse_blast_result.py

--- Parse_blast_result.py
+++ Parse_blast_result.py
@@ -10,12 +10,12 @@ from math import log
 f1 = open(sys.argv[1],'r')
 f2 = open(sys.argv[2],'w')
 
-f2.write('Gene_ID' + '\t' + 'HGT_evalue' + '\t' + 'Algae_evalue' + '\t' + 'Non_plant_evalue' + '\t' + 'HGT_score' + '\t' + 'Algae_score' + '\t' + 'Non_plant_score' + '\t' + 'HGT_identify' + '\t' + 'Algae_identify' + '\t' + 'Non_plant_identify' + '\n')
+f2.write('Gene_ID' + '\t' + 'HGT_evalue' + '\t' + 'A_evalue' + '\t' + 'B_evalue' + '\t' + 'HGT_score' + '\t' + 'A_score' + '\t' + 'B_score' + '\t' + 'HGT_identify' + '\t' + 'A_identify' + '\t' + 'B_identify' + '\n')
 
 List1 = []
-last_Evalue_P,last_Evalue_N = 1,1
-last_score_P, last_score_N = 0,0
-last_identify_P, last_identify_N = 0,0
+last_Evalue_A,last_Evalue_B = 1,1
+last_score_A, last_score_B = 0,0
+last_identify_A, last_identify_B = 0,0
 for line in f1:
     sply = line.split()
     ID1 = sply[0]
@@ -26,38 +26,39 @@ for line in f1:
     if len(List1) == 0:
         List1.append(ID1)
         if ID2[0] == 'A':
-            last_Evalue_P = Evalue
-            last_score_P = Score
-            last_identify_P = Identify
+            last_Evalue_A = Evalue
+            last_score_A = Score
+            last_identify_A = Identify
         if ID2[0] != 'A':
-            last_Evalue_N = Evalue
-            last_score_N = Score
-            last_identify_N = Identify
+            last_Evalue_B = Evalue
+            last_score_B = Score
+            last_identify_B = Identify
     else:
         if ID1 in List1:
             if ID2[0] == 'A':
-                last_Evalue_P = min(Evalue,last_Evalue_P)
-                last_score_P = max(Score,last_score_P)
-                last_identify_P = max(Identify,last_identify_P)
+                last_Evalue_A = min(Evalue,last_Evalue_A)
+                last_score_A = max(Score,last_score_A)
+                last_identify_A = max(Identify,last_identify_A)
             if ID2[0] != 'A':
-                last_Evalue_N = min(Evalue,last_Evalue_N)
-                last_score_N = max(Score,last_score_N)
-                last_identify_N = max(Identify,last_identify_N)
+                last_Evalue_B = min(Evalue,last_Evalue_B)
+                last_score_B = max(Score,last_score_B)
+                last_identify_B = max(Identify,last_identify_B)
         if ID1 not in List1:
-            HGT_AI = math.log(last_Evalue_P + 1e-180) - math.log(last_Evalue_N + 1e-180)
-            HGT_Index = last_score_N - last_score_P
-            HGT_Identify = last_identify_N - last_identify_P
-            f2.write(last_ID1 + '\t' + str(HGT_AI) + '\t' + str(last_Evalue_P) + '\t' + str(last_Evalue_N) + '\t' + str(HGT_Index) + '\t' + str(last_score_P) + '\t' + str(last_score_N) + '\t' + str(HGT_Identify) + '\t' + str(last_identify_P) + '\t' + str(last_identify_N) + '\n')
+            HGT_AI = math.log(last_Evalue_A + 1e-180) - math.log(last_Evalue_B + 1e-180)
+            HGT_Index = last_score_B - last_score_A
+            HGT_Identify = last_identify_B - last_identify_A
+            f2.write(last_ID1 + '\t' + str(HGT_AI) + '\t' + str(last_Evalue_A) + '\t' + str(last_Evalue_B) + '\t' + str(HGT_Index) + '\t' + str(last_score_A) + '\t' + str(last_score_B) + '\t' + str(HGT_Identify) + '\t' + str(last_identify_A) + '\t' + str(last_identify_B) + '\n')
             List1.append(ID1)
             if ID2[0] == 'A':
-                last_Evalue_P,last_Evalue_N = Evalue,1
-                last_score_P,last_score_N = Score,0
-                last_identify_P,last_identify_N = Identify,0
+                last_Evalue_A,last_Evalue_B = Evalue,1
+                last_score_A,last_score_B = Score,0
+                last_identify_A,last_identify_B = Identify,0
             if ID2[0] != 'A':
-                last_Evalue_N,last_Evalue_P = Evalue,1
-                last_score_N,last_score_P = Score,0
-                last_identify_N,last_identify_P = Identify,0
+                last_Evalue_B,last_Evalue_A = Evalue,1
+                last_score_B,last_score_A = Score,0
+                last_identify_B,last_identify_A = Identify,0
     last_ID1 = ID1
-if sply != '': f2.write(last_ID1 + '\t' + str(math.log(last_Evalue_P + 1e-180) - math.log(last_Evalue_N + 1e-180)) + '\t' + str(last_Evalue_P) + '\t' + str(last_Evalue_N) + '\t' + str(last_score_N - last_score_P) + '\t' + str(last_score_P) + '\t' + str(last_score_N) + '\t' + str(last_identify_N - last_identify_P) + '\t' + str(last_identify_P) + '\t' + str(last_identify_N) + '\n') # make sure don't miss the last one when parsing
+if sply != '': f2.write(last_ID1 + '\t' + str(math.log(last_Evalue_A + 1e-180) - math.log(last_Evalue_B + 1e-180)) + '\t' + str(last_Evalue_A) + '\t' + str(last_Evalue_B) + '\t' + str(last_score_B - last_score_A) + '\t' + str(last_score_A) + '\t' + str(last_score_B) + '\t' + str(last_identify_B - last_identify_A) + '\t' + str(last_identify_A) + '\t' + str(last_identify_B) + '\n') # make sure don't miss the last one when parsing
+
 f1.close()
 f2.close()
