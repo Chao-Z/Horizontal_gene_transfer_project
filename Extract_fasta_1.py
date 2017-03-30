edd559681aa145abb19136e77afe3622c78ba4ca
Chao-Z <Chao-Z@users.noreply.github.com>
2017-03-30 21:10:19 +0800

    Add files via upload

--- Extract_fasta_1.py
+++ Extract_fasta_1.py
@@ -8,7 +8,7 @@ f3 = open(sys.argv[3],'r')
 
 All_list = set((line.split()[0] for line in f3))
 Check_id_list = []
-Non_p = []
+B_id = []
 in_accession_ids = False
 Last_ID,Final_ID = '',''
 # function for parse fasta sequences in id and sequences
@@ -30,43 +30,43 @@ for line in f2:
     if ID1 in All_list:
         if len(Check_id_list) == 0:
             Check_id_list.append(ID1)
-            if ID2[0] != 'B':
-                Non_p.append(ID2)
+            if ID2[0] != 'A':
+                B_id.append(ID2)
         if ID1 in Check_id_list:
             Final_ID = ID1
-            if ID2 not in Non_p and ID2[0] != 'B':
-                Non_p.append(ID2)
+            if ID2 not in B_id and ID2[0] != 'A':
+                B_id.append(ID2)
         if ID1 not in Check_id_list:
             Check_id_list.append(ID1)
             New_file = open(Last_ID + '.fa','w')
-            New_non_p = []
-            if len(Non_p) >= 100:
-                New_non_p = Non_p[0:100]
+            New_B_id = []
+            if len(B_id) >= 100:
+                New_B_id = B_id[0:100]
             else:
-                New_non_p = Non_p
+                New_B_id = B_id
             with open(sys.argv[1]) as files:
                 for name,seq in read_fasta(files):
                     accessorID = name.split()[0]
                     accessorID_1 = accessorID[1:]
-                    in_accession_ids = accessorID_1 in New_non_p
+                    in_accession_ids = accessorID_1 in New_B_id
                     if in_accession_ids:
                         print >> New_file, name
                         print >> New_file, seq
-            Non_p = []
-            if ID2[0] != 'B':
-                Non_p.append(ID2)
+            B_id = []
+            if ID2[0] != 'A':
+                B_id.append(ID2)
         Last_ID = ID1
 New_file = open(Final_ID + '.fa','w')
-New_non_p = []
-if len(Non_p) >= 100:
-    New_non_p = Non_p[0:100]
+New_B_id = []
+if len(B_id) >= 100:
+    New_B_id = B_id[0:100]
 else:
-    New_non_p = Non_p
+    New_B_id = B_id
 with open(sys.argv[1]) as files:
     for name,seq in read_fasta(files):
         accessorID = name.split()[0]
         accessorID_1 = accessorID[1:]
-        in_accession_ids = accessorID_1 in New_non_p
+        in_accession_ids = accessorID_1 in New_B_id
         if in_accession_ids:
             print >> New_file, name
             print >> New_file, seq
